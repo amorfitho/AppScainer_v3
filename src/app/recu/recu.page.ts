9c483d93e1b4febfa8ae6d030331047bf1d26ab9
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-10-24 21:37:14 -0300

    vuelta recu
    deje recu como esatba

--- src/app/recu/recu.page.ts
+++ src/app/recu/recu.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recu',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuPage implements OnInit {
 
-  constructor() { }
+  formularioRecu: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    public fb: FormBuilder, 
+    public alertController: AlertController, 
+    public navControl: NavController
+  ) { 
+    this.formularioRecu = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'email': new FormControl("", [Validators.required, Validators.email]), // Valida que sea un email válido
+      'newpassword': new FormControl("", [Validators.required, Validators.minLength(6)]), // Contraseña de mínimo 6 caracteres
+      'confirmpassword': new FormControl("", Validators.required)
+    });
   }
 
+  ngOnInit() { }
+
+  async actualizar() {
+    var f = this.formularioRecu.value;
+    const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
+    
+    // Verificamos que el formulario esté lleno correctamente y las contraseñas coincidan
+    if (f.newpassword !== f.confirmpassword) {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Las contraseñas no coinciden.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+
+    if (f.nombre === usuario.nombre && f.email === usuario.email) {
+     
+      var updateUsuario = {
+        nombre: f.nombre,
+        apellidos: usuario.apellidos,
+        email: f.email,
+        password: f.newpassword
+      };
+
+      
+      localStorage.setItem('usuario', JSON.stringify(updateUsuario));
+      localStorage.setItem('Actualizado', 'true');
+      
+      
+      this.navControl.navigateRoot('login');
+
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Los datos no coinciden con los del usuario actual.',
+        buttons: ['OK']
+      });
+      await alert.present();
+      return;
+    }
+  }
 }
+
