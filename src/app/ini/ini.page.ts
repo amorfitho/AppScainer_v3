f52c59484e14d3e16cc9facf2196804086e20472
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-10-24 21:32:08 -0300

    vuelta ini
    deje ini como estaba antes

--- src/app/ini/ini.page.ts
+++ src/app/ini/ini.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ini',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniPage implements OnInit {
 
-  constructor() { }
+  nombre: string = '';
+  apellidos: string = '';
+  email: string = '';
+
+
+  constructor(public navControl: NavController) {}
 
   ngOnInit() {
   }
 
+  datosentrada() {
+    
+    const usuario = JSON.parse(localStorage.getItem('usuario') || '{}');
+    
+    this.nombre = usuario.nombre || 'Usuario';
+    this.apellidos = usuario.apellidos || 'apellido paterno apellido materno';
+    this.email = usuario.email || 'email@gmai.cl';
+  }
+
+  async salir() {
+    localStorage.removeItem('Ingresado');
+    console.log('Sesión cerrada');
+    // Redirige al usuario a la página de login después de cerrar sesión
+    this.navControl.navigateRoot('login');
+  }
+  //quiero suicidarme
 }
