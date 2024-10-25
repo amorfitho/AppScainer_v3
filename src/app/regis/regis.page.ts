6d9b5db2def6b9687f2ac6e51e917da16d4deb53
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-10-24 21:41:22 -0300

    vuelta regis
    y algunas cositas que faltaron

--- src/app/regis/regis.page.ts
+++ src/app/regis/regis.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,FormBuilder,Validators, EmailValidator} from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-regis',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisPage implements OnInit {
 
-  constructor() { }
+  formularioRegis: FormGroup;
+
+  constructor(public fb: FormBuilder, public alertController: AlertController, public navControl: NavController ) { 
+
+    this.formularioRegis = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'apellidos': new FormControl("",Validators.required),
+      'email': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required),
+      'confirmarPassword': new FormControl("",Validators.required)
+    });
+  }
 
   ngOnInit() {
   }
+
+  async guardar(){
+    var f = this.formularioRegis.value;
+
+    if (this.formularioRegis.invalid){
+      const alert = await this.alertController.create({
+        header: 'Algo te falto',
+        subHeader: 'Subtitle',
+        message: 'Tienes que llenar todos los campos.',
+        buttons: ['Ta Bien']
+      })
+
+      await alert.present();
+      return;
+      
+    }
+
+    var usuario ={
+      nombre: f.nombre,
+      apellidos: f.apellidos,
+      email: f.email,
+      password: f.password
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+
+    localStorage.setItem('Registrado','true');
+      this.navControl.navigateRoot('');
+
+  }
+
+  
 
 }
