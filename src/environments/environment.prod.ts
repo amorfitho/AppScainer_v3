242e23e24ec582056abe0732725065e849dbe5b5
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-11-13 18:30:43 -0300

    actualizacion app
    la puta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //apiURL:"http://localhost:3000"
-  apiURL:"https://29dv2vj2-3000.brs.devtunnels.ms"
+  apiURL:"https://l31zkbq2-3000.brs.devtunnels.ms"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiURL:"http://localhost:3000"
-  apiURL:"https://29dv2vj2-3000.brs.devtunnels.ms"
+  apiURL:"https://l31zkbq2-3000.brs.devtunnels.ms"
 
 };
 
