7f52e3dd0091c94293138e4ba86344c161bc8916
Fabian <119624256+amorfitho@users.noreply.github.com>
2024-11-24 20:40:16 -0300

    se termiano
    esat todo listo ahora no guarda el qr pero bueno

--- src/app/skainer/skainer.page.ts
+++ src/app/skainer/skainer.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, Platform } from '@ionic/angular';
+import { LoadingController, ModalController, Platform } from '@ionic/angular';
 import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 import { BarcodeScanner, LensFacing } from '@capacitor-mlkit/barcode-scanning';
 import { Capacitor } from '@capacitor/core';
 import { FilePicker } from '@capawesome/capacitor-file-picker';
+import html2canvas from 'html2canvas';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+import { __classPrivateFieldGet } from 'tslib';
 
 @Component({
   selector: 'app-skainer',
@@ -20,7 +24,8 @@ export class SkainerPage implements OnInit {
 
   constructor(
     private modalController: ModalController,
-    private platform:Platform
+    private platform:Platform,
+    private loadingController:LoadingController
   ) { }
 
   ngOnInit(): void {
@@ -54,12 +59,73 @@ export class SkainerPage implements OnInit {
   }
 
 
-  async rearbarcodefromimage(){
+  async readBarcodesFromImage(){
     
-      const {files} = await FilePicker.pickImages({ });
+    const {files} = await FilePicker.pickImages({ });
 
-      const path = files[0]?.path
-      
+    const path = files[0]?.path;
+
+    if(!path) return;
+
+    const { barcodes } = await BarcodeScanner.readBarcodesFromImage({
+      path,
+      formats: []
+    })
     
+    this.resultadoSka = barcodes[0].displayValue;
+  }
+
+  //guardar qr
+  captureScreen(){
+
+    const element = document.getElementById('qrImg') as HTMLElement;
+    html2canvas(element).then((canvas: HTMLCanvasElement) =>{
+      this.downloadImagen(canvas);
+      if(this.platform.is('capacitor')) this.shareImagen(canvas);
+      else this.downloadImagen(canvas);
+    })
+  }
+
+  //descarar imagen en web
+  downloadImagen(canvas: HTMLCanvasElement){
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL();
+    link.download = 'qr.png';
+    link.click();
+  }
+
+  //descarar imagen en celu
+  async shareImagen(canvas: HTMLCanvasElement){
+    let base64 = canvas.toDataURL();
+    let path = 'qr.png';
+
+      const loading = await this.loadingController.create({
+        spinner: 'crescent'
+      });
+      await loading.present();
+    
+
+    await Filesystem.writeFile({
+      path,
+      data: base64,
+      directory: Directory.Cache,
+    }).then(async (res)=>{
+      let uri= res.uri
+      
+      
+
+      await Share.share({
+      url: 'uri'});
+
+      await Filesystem.deleteFile({
+        path,
+        directory: Directory.Cache
+      })
+
+
+      }).finally(()=>{
+        loading.dismiss();
+      })
+     
   }
 }
